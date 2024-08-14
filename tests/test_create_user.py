5dd622b39d8626137332d3abcabde700312f24d6
Platon <platon.tikhomirov@yandex.ru>
2024-08-14 23:04:29 +0300

    1

---
+++ tests/test_create_user.py
@@ -0,0 +1,15 @@
+import requests
+import allure
+import pytest
+
+#from conftest import generator
+#from conftest import create_user
+
+#from data import Endpoints
+#from data import FakeData
+
+class TestCreateUser:
+
+    def test_able_to_create_uder(self):
+
+        pass # подключить генератор
